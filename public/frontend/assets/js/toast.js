812b262de10869030d2fbaf14e8ac0af7a5ffc0a
Vaibhav Vegad <vevgad1525@gmail.com>
2024-08-29 10:12:23 +0530

    traker

---
+++ public/frontend/assets/js/toast.js
@@ -0,0 +1,9 @@
+toastr.options = {
+        closeButton: true,
+        progressBar: true,
+        // preventDuplicates: true,
+        positionClass: 'toast-top-right', // Example position
+        timeOut: 3000 // 3 seconds duration for toast messages
+    };
+    
+    
